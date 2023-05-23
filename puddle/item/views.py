07c52af7f78070c5b42f41c5e591d9e520f78a04
Alan Hendri N <71018935+alanhendrin@users.noreply.github.com>
2023-05-23 13:26:55 +0700

    update 20230523
    update new page

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Item
+def detail(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(batch_name=item.batch_name).exclude(pk=pk)[0:3]
+
+    return render(request, 'item/detail.html', {
+        'item': item,
+        'related_items': related_items
+    })
